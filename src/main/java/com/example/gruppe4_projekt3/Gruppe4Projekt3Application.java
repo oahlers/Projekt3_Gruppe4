dd2d6699395b3fc3a84013ffd558de1dbf00b07f
oahlers <oliv2527@stud.kea.dk>
2025-04-25 16:27:18 +0200

    #1

---
+++ src/main/java/com/example/gruppe4_projekt3/Gruppe4Projekt3Application.java
@@ -0,0 +1,13 @@
+package com.example.gruppe4_projekt3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gruppe4Projekt3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gruppe4Projekt3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gruppe4_projekt3/Gruppe4Projekt3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gruppe4_projekt3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gruppe4Projekt3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
