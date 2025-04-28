a83c1d84e67f9639fbd601eb6fc7c9ae8a543b56
rasmusguldborg <rap00004@stud.kea.dk>
2025-04-28 10:56:25 +0200

    ff

--- src/main/java/com/example/gruppe4_projekt3/model/Employee.java
+++ src/main/java/com/example/gruppe4_projekt3/model/Employee.java
@@ -2,8 +2,40 @@ package com.example.gruppe4_projekt3.model;
 
 public class Employee {
     private int employeeId;
-    private String fullName
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String fullName;
     private String username;
     private String password;
-
 }
