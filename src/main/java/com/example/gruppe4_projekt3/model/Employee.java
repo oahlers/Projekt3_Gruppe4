acbfdd5e2975187861328ac19ce5daf94b6e7848
rasmusguldborg <rap00004@stud.kea.dk>
2025-04-28 10:43:41 +0200

    ff

--- src/main/java/com/example/gruppe4_projekt3/model/Employee.java
+++ src/main/java/com/example/gruppe4_projekt3/model/Employee.java
@@ -2,9 +2,7 @@ package com.example.gruppe4_projekt3.model;
 
 public class Employee {
     private int employeeId;
-    private String firstname;
-    private String lastname;
-    private String userType;
+    private String fullName
     private String username;
     private String password;
 
