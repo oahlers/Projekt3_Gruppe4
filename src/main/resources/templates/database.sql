1a0dfaa31bfa96ebcc5f0ef9f51c5eb34971dc8f
rasmusguldborg <rap00004@stud.kea.dk>
2025-05-06 11:07:39 +0200

    Test af skaderapport

--- src/main/resources/templates/database.sql
+++ src/main/resources/templates/database.sql
@@ -25,8 +25,8 @@ VALUES
     (0, 2023, 'Fiat', '500e Icon Pack 118 HK', 'Hvid', 'Icon Pack', 'EVF50001', 'FIAT500EICON12345678', 259995.00, 16230.00, b'0', b'0', 5),
     (100, 2023, 'Tesla', 'Model Y Long Range', 'Blå', 'Standard', 'EVT10001', 'TESLAMODELYLONG12345678', 689995.00, 29550.00, b'0', b'0', 8),
     (50, 2023, 'BMW', 'iX3', 'Grå', 'Xdrive', 'EVB50001', 'BMWIX3123456789', 799995.00, 41250.00, b'0', b'0', 10),
-    (0, 2022, 'Nissan', 'Leaf 40 kWh', 'Rød', 'N-Connecta', 'EVN20001', 'NISSANLEAF12345678', 219995.00, 11850.00, b'0', b'0', 6),
-    (0, 2021, 'Renault', 'Zoe 52 kWh', 'Grøn', 'Intens', 'EVR50001', 'RENAULTZOE12345678', 189995.00, 8200.00, b'0', b'0', 5),
+    (0, 2022, 'Nissan', 'Leaf 40 kWh', 'Rød', 'N-Connecta', 'EVN20001', 'NISSANLEAF12345678', 219995.00, 11850.00, b'0', b'1', 6),
+    (0, 2021, 'Renault', 'Zoe 52 kWh', 'Grøn', 'Intens', 'EVR50001', 'RENAULTZOE12345678', 189995.00, 8200.00, b'0', b'1', 5),
     (0, 2023, 'Audi', 'Q4 e-tron', 'Blå', 'S Line', 'EVA40001', 'AUDIQ4ETRON12345678', 619995.00, 23400.00, b'0', b'0', 7),
     (0, 2024, 'Volkswagen', 'ID.4', 'Sort', 'Pro Performance', 'EVW20001', 'VOLKSWAGENID4123456789', 479995.00, 20750.00, b'0', b'0', 6);
 
