a89a2ee743c7ede6325fd077eb0e9451aea0188a
oahlers <oliv2527@stud.kea.dk>
2025-05-14 09:08:26 +0200

    extra button

--- src/main/java/com/example/gruppe4_projekt3/controller/DamageReportController.java
+++ src/main/java/com/example/gruppe4_projekt3/controller/DamageReportController.java
@@ -7,6 +7,8 @@ import com.example.gruppe4_projekt3.repository.CarRepository;
 import com.example.gruppe4_projekt3.repository.DamageReportRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -47,4 +49,5 @@ public class  DamageReportController {
 
         return "damageReportDone";
     }
+
 }
