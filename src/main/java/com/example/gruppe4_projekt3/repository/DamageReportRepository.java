b6082bde55f2fcbee27749a6f114cdd9b16fa889
rasmusguldborg <rap00004@stud.kea.dk>
2025-05-15 12:21:54 +0200

    Register and deliver car fix

--- src/main/java/com/example/gruppe4_projekt3/repository/DamageReportRepository.java
+++ src/main/java/com/example/gruppe4_projekt3/repository/DamageReportRepository.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 @Repository
-public class DamageReportRepository {
+public class  DamageReportRepository {
 
     private final JdbcTemplate jdbcTemplate;
     private final CarRepository carRepository;
